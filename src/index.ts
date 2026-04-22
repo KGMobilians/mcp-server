78c182cc973025d44c53bc7b5eca68b3b69207db
finalgod <finalgod@naver.com>
2026-04-22 19:31:47 +0900

    Bump version to 2.0.5

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import { registerGetNezoCodeExampleTool } from "./tool/get-nezo-code-example.js"
 
 const server = new McpServer({
   name: "mobilpay",
-  version: "2.0.4",
+  version: "2.0.5",
 });
 
 await buildIndex();
